603e5df14bad1767b3636b34629b891fa143fcc3
Mike_Zhang <Mike_Zhang@live.com>
2017-06-16 18:07:51 +0800

    add sshTools/loginUsePwdTest2.py

---
+++ sshTools/loginUsePwdTest2.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*- 
+
+import pexpect 
+from getpass import getpass
+
+#passwd = getpass()
+passwd = "123456"
+
+def ssh_cmd(user,ip, cmd): 
+    ssh = pexpect.spawn('ssh %s@%s "%s"' % (user,ip,cmd)) 
+    try: 
+        i = ssh.expect(['password:', 'continue connecting (yes/no)?'],timeout=5) 
+        if i == 0 : 
+            ssh.sendline(passwd) 
+        elif i == 1: 
+            ssh.sendline('yes') 
+            ssh.expect('password: ') 
+            ssh.sendline(passwd) 
+    except pexpect.EOF: 
+        print "EOF" 
+    except pexpect.TIMEOUT:
+        print "TIMEOUT"
+    else:
+        r = ssh.read() 
+        print r
+    ssh.close()
+
+if __name__ == '__main__':
+    ssh_cmd("root","192.168.1.100","ifconfig")
+
+   
