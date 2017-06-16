f94a2e307e02afd630c949f8d42fb9c42ca811e2
Mike_Zhang <Mike_Zhang@live.com>
2017-06-16 17:52:03 +0800

    add sshTools/loginUsePwdTest1.py

---
+++ sshTools/loginUsePwdTest1.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python 
+#-*- coding:utf-8 -*-
+
+import warnings 
+
+with warnings.catch_warnings():
+    warnings.simplefilter("ignore")
+    import paramiko 
+ 
+hostname='192.168.1.100' 
+username='root' 
+password='123456' 
+
+#paramiko.util.log_to_file('paramiko.log')
+s=paramiko.SSHClient() 
+#s.load_system_host_keys() 
+s.set_missing_host_key_policy(paramiko.AutoAddPolicy()) 
+s.connect(hostname = hostname,username=username, password=password) 
+stdin,stdout,stderr=s.exec_command('ifconfig;free;df -h')     
+print stdout.read() 
+s.close() 
+raw_input()
