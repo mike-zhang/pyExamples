5390482c336140f34380869ec9dcb2a992cfb924
Mike_Zhang <Mike_Zhang@live.com>
2017-06-16 11:56:21 +0800

    add algorithm/dataMining_KNN

---
+++ algorithm/dataMining_KNN/knn_sklearn_test1.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python
+#-*- coding:utf-8 -*-
+
+from sklearn import neighbors  
+import numpy
+
+data = [
+    numpy.array([1.0, 1.1]),
+    numpy.array([1.0, 1.0]),
+    numpy.array([0.0, 0.0]),
+    numpy.array([0.0, 0.1]),
+    numpy.array([1.3, 1.1])
+]
+labels = ['A','A','B','B','A'] 
+
+knn = neighbors.KNeighborsClassifier(n_neighbors=3)  
+knn.fit(data, labels)  
+
+print knn.predict(numpy.array([1.2, 1.1]).reshape(1,-1))  
+print knn.predict(numpy.array([0.2, 0.1]).reshape(1,-1))  
---
+++ algorithm/dataMining_KNN/knn_test1.py
@@ -0,0 +1,41 @@
+#! /usr/bin/env python
+#-*- coding:utf-8 -*-
+
+import math
+
+class KNN:    
+    def __init__(self,trainData,trainLabel,k):
+        self.trainData = trainData
+        self.trainLabel = trainLabel
+        self.k = k       
+    
+    def predict(self,inputPoint):
+        retLable = "None"
+        arr=[]
+        for vector,lable in zip(self.trainData,self.trainLabel):
+            s = 0
+            for i,n in enumerate(vector) :
+                s += (n-inputPoint[i]) ** 2
+            arr.append([math.sqrt(s),lable])
+        arr = sorted(arr,key=lambda x:x[0])[:self.k]           
+        dtmp = {}
+        for k,v in arr :
+            if not v in dtmp : dtmp[v]=0
+            dtmp[v] += 1
+        retLable,_ = sorted(dtmp.items(),key=lambda x:x[1],reverse=True)[0]        
+        return retLable
+
+data = [
+    [1.0, 1.1],
+    [1.0, 1.0],
+    [0.0, 0.0],
+    [0.0, 0.1],
+    [1.3, 1.1],
+]
+
+labels = ['A','A','B','B','A'] 
+knn = KNN(data,labels,3)
+
+print knn.predict([1.2, 1.1])  
+print knn.predict([0.2, 0.1])  
+
