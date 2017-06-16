c02dc7c818c81e164116502ebb5daedc84a66e07
Mike_Zhang <Mike_Zhang@live.com>
2017-06-16 22:36:04 +0800

    add loginUsePriKeyTest1.py

---
+++ sshTools/loginUsePriKeyTest1.py
@@ -0,0 +1,22 @@
+﻿#! /usr/bin/env python 
+#-*- coding:utf-8 -*-
+
+import warnings 
+
+with warnings.catch_warnings():
+    warnings.simplefilter("ignore")
+    import paramiko 
+ 
+hostname='192.168.1.100' 
+username='root' 
+keyFilePath = "./rsaPri" # openssh private key file
+port=22 
+if __name__=='__main__':  
+    key=paramiko.RSAKey.from_private_key_file(keyFilePath) 
+    s=paramiko.SSHClient()
+    s.load_system_host_keys()
+    s.connect(hostname,port,username,pkey=key)
+    stdin,stdout,stderr=s.exec_command('ifconfig')
+    print stdout.read()
+    s.close()
+    
