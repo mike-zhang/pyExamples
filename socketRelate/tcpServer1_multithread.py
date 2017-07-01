f1dbaf3aefd48b9bb8693b5ae5732a15f1b0b439
Mike_Zhang <Mike_Zhang@live.com>
2017-07-01 22:31:38 +0800

    add socketRelate/tcpServer1_multithread.py

---
+++ socketRelate/tcpServer1_multithread.py
@@ -0,0 +1,46 @@
+#! /usr/bin/env python
+#-*- coding:utf-8 -*-
+
+import socket
+import threading
+
+class ThreadedServer(object):
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.sock.bind((self.host, self.port))
+
+    def listen(self):
+        self.sock.listen(5)
+        while True:
+            client, address = self.sock.accept()
+            client.settimeout(60)
+            threading.Thread(target = self.listenToClient,args = (client,address)).start()
+
+    def listenToClient(self, client, address):
+        size = 1024
+        while True:
+            try:
+                data = client.recv(size)
+                if data:                    
+                    response = data
+                    client.send(response)
+                    print "secndLen: ",len(data)                    
+                else:
+                    raise error('Client disconnected')
+            except:
+                client.close()
+                return False
+
+if __name__ == "__main__":
+    while True:
+        port_num = 12345
+        try:
+            port_num = int(port_num)
+            break
+        except ValueError:
+            pass
+
+    ThreadedServer('',port_num).listen()
