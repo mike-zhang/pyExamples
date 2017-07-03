8c2f852c97d8d6385cf565cd801448e1000d7b69
Mike_Zhang <Mike_Zhang@live.com>
2017-07-03 11:29:46 +0800

    add sync script

---
+++ doPull.py
@@ -0,0 +1,9 @@
+﻿#! /usr/bin/env python
+#-*- coding: utf-8 -*-
+# version : Python 2.7.13
+
+import os
+
+strCmd = "git pull"
+os.system(strCmd)
+
---
+++ doPush.py
@@ -0,0 +1,22 @@
+﻿#! /usr/bin/env python
+#-*- coding: utf-8 -*-
+# version : Python 2.7.13
+
+import os
+
+def doGitPush():
+    bNameAll = os.popen("git branch -l").read()
+    #print bNameAll
+    bName = ""
+    for item in bNameAll.split('\n'):            
+        if item[0] == '*' :
+            bName = item
+            break
+    #print bName
+    bName = bName.split()[-1]
+    print bName
+    strCmd = "git push origin %s" % bName
+    print strCmd
+    os.system(strCmd)
+
+doGitPush()
