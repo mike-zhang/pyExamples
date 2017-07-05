e2e8191a39682cb345a105d4866dbdd97c906458
Mike_Zhang <Mike_Zhang@live.com>
2017-07-05 13:32:03 +0800

    add intTime2Str.py strTime2Int.py

---
+++ timeDateRelate/intTime2Str.py
@@ -0,0 +1,25 @@
+﻿#! /usr/bin/env python 
+#-*- coding:utf-8 -*- 
+# version : Python 2.7.13
+
+import time,datetime,re,dateutil
+import pandas as pd
+
+def intTime2Str(unixTimeInt):
+    if unixTimeInt < 0 : unixTimeInt = 0
+    return time.strftime("%Y-%m-%d %H:%M:%S",time.localtime(unixTimeInt))    
+
+def intTime2Str_2(unixTimeInt):    
+    if unixTimeInt < 0 : unixTimeInt = 0
+    return datetime.datetime.fromtimestamp(unixTimeInt).strftime("%Y-%m-%d %H:%M:%S")
+    
+def intTime2Str_3(unixTimeInt):        
+    if unixTimeInt < 0 : unixTimeInt = 0
+    return pd.datetime.fromtimestamp(unixTimeInt).strftime("%Y-%m-%d %H:%M:%S")    
+  
+t1 = time.time()
+print t1   
+print "intTime2Str : ",intTime2Str(t1)
+print "intTime2Str_2 : ",intTime2Str_2(t1)
+print "intTime2Str_3 : ",intTime2Str_3(t1)
+
---
+++ timeDateRelate/strTime2Int.py
@@ -0,0 +1,28 @@
+﻿#! /usr/bin/env python 
+#-*- coding:utf-8 -*- 
+# version : Python 2.7.13
+
+import time,datetime,re,dateutil
+import pandas as pd
+
+patternStrTime = re.compile('(\d{4})-(\d{2})-(\d{2}) (\d{2}):(\d{2}):(\d{2})')
+def strTime2Int(strTime):            
+    #patternStrTime = re.compile('(\d{4})-(\d{2})-(\d{2}) (\d{2}):(\d{2}):(\d{2})')    
+    if not patternStrTime.match(strTime) :     
+        #print "\"%s\" is not time string format" % strTime
+        return 0        
+    return int(time.mktime(datetime.datetime(* map(int,patternStrTime.search(strTime).groups()) ).timetuple()))    
+
+def strTime2Int_2(strTime):            
+    ts=pd.to_datetime(strTime)
+    return ts.value/(10**9)
+   
+def strTime2Int_3(strTime):
+    ts = time.mktime(dateutil.parser.parse(strTime).timetuple())
+    return int(ts)
+
+s1 = '2017-07-05 00:00:00'
+print s1   
+print "strTime2Int : ",strTime2Int(s1)
+print "strTime2Int_2 : ",strTime2Int_2(s1)
+print "strTime2Int_3 : ",strTime2Int_3(s1)
