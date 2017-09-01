6eae59b27ca16fa044d8d0a9f67d65749c25d554
Mike_Zhang <Mike_Zhang@live.com>
2017-09-01 11:13:13 +0800

    add timeDateRelate/getNextMonthTest1.py

---
+++ timeDateRelate/getNextMonthTest1.py
@@ -0,0 +1,13 @@
+﻿#! /usr/bin/python
+#-*- coding: utf-8 -*-  
+
+# get same day of next month  
+
+from dateutil import relativedelta
+import datetime
+
+d1 = datetime.date.today().replace(month=12,day=1)
+print d1
+d2 = d1 + relativedelta.relativedelta(months=1)
+
+print d2
