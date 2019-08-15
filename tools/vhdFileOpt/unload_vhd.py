dce9bb582bb5ba8a3f6041b34672bc0333b84dd7
Mike_Zhang <Mike_Zhang@live.com>
2019-08-15 21:37:02 +0800

    add tools/vhdFileOpt

---
+++ tools/vhdFileOpt/unload_vhd.py
@@ -0,0 +1,10 @@
+from subprocess import Popen,PIPE
+
+# env : windows 7
+def load_vhd(file):
+    p1 = Popen("diskpart",stdin=PIPE)
+    p1.stdin.write('''select vdisk file="%s"\r\n'''%file)
+    p1.stdin.write('''detach vdisk \r\n''')
+
+load_vhd(r"D:\workspace\srcRead.vhd")    
+
